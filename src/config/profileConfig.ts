b101851aacf04ca78a260c3cf40cffaac1243b05
Eysnter <czk246310@gmail.com>
2026-05-27 17:09:36 +0800

    更新

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.png",
+	avatar: "assets/images/zi.png",
 
 	// 名字
-	name: "Firefly",
+	name: "Eysnter",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm Eysnter.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,7 +24,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/2532439877",
 			showName: false,
 		},
 		{
@@ -36,7 +36,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:czk246310@gmail.com",
 			showName: false,
 		},
 		{
---
+++ src/content/posts/xing.md
@@ -0,0 +1,12 @@
+---
+title: xing.md
+published: 2026-05-27
+description: ''
+image: ''
+tags: []
+category: ''
+draft: false 
+lang: ''
+---
+
+一篇新文章

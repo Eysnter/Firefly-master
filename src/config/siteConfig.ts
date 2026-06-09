caf048ecc1016d25ac5d1af97715f14885cbb2bc
Eysnter <czk246310@gmail.com>
2026-06-09 21:47:53 +0800

    更新 daemon.pid 中的进程 ID 和启动时间，更新 siteConfig.ts 中的 Google Analytics ID

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -173,7 +173,7 @@ export const siteConfig: SiteConfig = {
   // 统计分析
   analytics: {
     // Google Analytics ID
-    googleAnalyticsId: "",
+    googleAnalyticsId: "G-T32ZYMC7LT",
     // Microsoft Clarity ID
     microsoftClarityId: "",
     // Umami 统计配置

e2a4b352f4e4e84966c1d4a52880990e1d198a8b
Eysnter <czk246310@gmail.com>
2026-06-11 18:05:30 +0800

    友链更新

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -78,6 +78,15 @@ export const friendsConfig: FriendLink[] = [
     weight: 10,
     enabled: true,
   },
+  {
+    title: "Hyde Blog",
+    imgurl: "https://seasir.top/assets/avatar.avif",
+    desc: "人心中的成见是一座大山",
+    siteurl: "https://seasir.top/",
+    tags: ["Blog"],
+    weight: 10,
+    enabled: true,
+  },
 ];
 
 
